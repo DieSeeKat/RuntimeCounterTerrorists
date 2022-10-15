f92092796e8941337608f8089ece1ec4c19af292
DvZ <danielvzyl1469@gmail.com>
2022-10-15 12:35:19 +0200

    Done with: first, next and isDone

--- Iterator/NodeIterator.cpp
+++ Iterator/NodeIterator.cpp
@@ -1,26 +1,38 @@
 #include "NodeIterator.h"
 #include "Path.h"
 
+int path_index = 0;
+
 NodeIterator::NodeIterator(Path **_paths, int num_paths)
 {
   paths = _paths;
+  path_count = num_paths;
 }
 
 void NodeIterator::first()
 {
-  Node **all_end_points = paths[0]->getEndPoints();
+  Node **first_end_points = paths[path_index]->getEndPoints();
+  current_node = first_end_points[1];
+  path_index++;
 }
 
 void NodeIterator::next()
 {
-  // TODO - implement NodeIterator::next
-  throw "Not yet implemented";
+  Node **next_end_points = paths[path_index]->getEndPoints();
+  current_node = next_end_points[1];
+  path_index++;
 }
 
 bool NodeIterator::isDone()
 {
-  // TODO - implement NodeIterator::isDone
-  throw "Not yet implemented";
+  if(path_index == this->path_count)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
 }
 
 Node *NodeIterator::currentNode()
