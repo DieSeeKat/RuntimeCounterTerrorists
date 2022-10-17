0ed832f7ed85582887fda6c170bdf152116d3b48
DvZ <danielvzyl1469@gmail.com>
2022-10-17 18:01:05 +0200

    Removed node_list, since iterator takes care of it.

--- Mediator/Mediator.h
+++ Mediator/Mediator.h
@@ -1,6 +1,6 @@
 #ifndef MEDIATOR_H
 #define MEDIATOR_H
-
+//In my implementation I assume the capitol node will be the mediator. When an empire gets attacked its capitol is aware of this and notify all the surrounding towns.
 class Node;
 
 class Mediator
