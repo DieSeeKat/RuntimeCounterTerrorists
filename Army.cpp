991d39d7ba1829fff945b0a84f69d13288b89770
Lukas Anthonissen <lukasanthonissen@gmail.com>
2022-10-18 15:29:25 +0200

    Add Doxygen documentation to Army, ArmyRatio and update Doxyfile to exclude Google tests

--- Army.cpp
+++ Army.cpp
@@ -17,12 +17,6 @@ void Army::moveToTown(Node *town)
   // TODO - implement Army::moveToTown
   throw "Not yet implemented";
 }
-
-void Army::defendTown(Node *town)
-{
-  // TODO - implement Army::defendTown
-  throw "Not yet implemented";
-}
 Node *Army::getPosition()
 {
   return position;
--- Army.h
+++ Army.h
@@ -14,19 +14,32 @@ class Army : Observer
   private:
   std::string observer_state;
   Node *subject;
+  /// The current Node at which the Army is stationed
   Node *position;
+  /// The number of resources that the Army has
   int resources;
+  /// The controlling Empire of the Army
   Empire *empire;
 
   public:
   void update();
 
+  /**
+   * @brief A method to allow the Army to siege a Node
+   * @param town The town which will be attacked
+   */
   void attackTown(Node *town);
 
+  /**
+   * @brief A method to move an Army to another Node
+   * @param town The town which will be moved to
+   */
   void moveToTown(Node *town);
 
-  void defendTown(Node *town);
-
+  /**
+   * @brief Return the current position of the Army
+   * @return Returns a Node pointer
+   */
   Node *getPosition();
 };
 
