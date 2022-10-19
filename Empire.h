b9b36b89c2e724d3ce94b0cb0a8c2944815bd0b1
DvZ <danielvzyl1469@gmail.com>
2022-10-19 09:33:38 +0200

    Changed empire's alliance vector to Alliance components

--- Empire.h
+++ Empire.h
@@ -34,7 +34,7 @@ class Empire : public AllianceComponent
   RecruitmentPolicy *recruitment_policy;
   /// The current stage of war (Attacking or Defending)
   WarStage *war_stage;
-  std::vector<Alliance *>alliances;
+  std::vector<AllianceComponent *>alliances;
   /// A vector of all controlled armies
   std::vector<Army> armies;
   /// A pointer to the War object, which hold information on all other entities in the war
