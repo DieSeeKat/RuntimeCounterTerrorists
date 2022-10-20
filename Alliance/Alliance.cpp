730b175900abfee9f3abd8b5e4ea746a339429b9
DvZ <danielvzyl1469@gmail.com>
2022-10-20 13:02:07 +0200

    Removed remove method

--- Alliance/Alliance.cpp
+++ Alliance/Alliance.cpp
@@ -7,20 +7,19 @@ void Alliance::add(AllianceComponent* alliance_component)
 
 void Alliance::remove(AllianceComponent* alliance_component)
 {
-  //find location of element to remove
-  int location = -1;
-  for(int i = 0; i < children.size(); i++)
-  {
-    if(children.at(i) == alliance_component)
-    {
-      location = i;
-    }
-  }
-
-  if(location != -1){
-    children.erase(children.begin()+ location);
-  }
-
+//  //find location of element to remove
+//  int location = -1;
+//  for(int i = 0; i < children.size(); i++)
+//  {
+//    if(children.at(i) == alliance_component)
+//    {
+//      location = i;
+//    }
+//  }
+//
+//  if(location != -1){
+//    children.erase(children.begin()+ location);
+//  }
 }
 
 std::vector<AllianceComponent*> Alliance::getChildren()
