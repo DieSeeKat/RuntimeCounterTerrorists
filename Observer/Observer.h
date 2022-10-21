ef70908d4f7d2f15257c44557479ac5ca7bb8915
Adam Osborne <u21529583@tuks.co.za>
2022-10-21 22:02:44 +0200

    Changes to Observer

--- Observer/Observer.h
+++ Observer/Observer.h
@@ -1,20 +1,15 @@
 #ifndef OBSERVER_H
 #define OBSERVER_H
 
-#include "Subject.h"
+
 
 class Observer
 {
 
     public:
-        Observer() : subject(0) {};
-
-        void registerSubject(Subject* subject);
 
         virtual void update() = 0;
 
-    protected:
-        Subject* subject;
 
 
 };
