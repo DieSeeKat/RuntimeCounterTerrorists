73422e2ccb08ee15e896bfefe811bd35659b4458
Adam Osborne <u21529583@tuks.co.za>
2022-10-21 20:36:52 +0200

    Added register function for Observer

--- Observer/Observer.h
+++ Observer/Observer.h
@@ -1,12 +1,18 @@
 #ifndef OBSERVER_H
 #define OBSERVER_H
 
+#include "Subject.h"
 
 class Observer
 {
 
     public:
         virtual void update() = 0;
+        void register(Subject* subject);
+
+    protected:
+
+        Subject* subject
 
 
 };
