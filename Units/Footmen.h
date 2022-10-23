1cd6127a2da4a654d75710f7280b4b8b2d489ad4
Liam <prinsloo.liam2@gmail.com>
2022-10-23 12:53:27 +0200

    Commiting slinger and footmen h files

---
+++ Units/Footmen.h
@@ -0,0 +1,24 @@
+#ifndef FOOTMEN_H
+#define FOOTMEN_H
+
+#include "Unit.h"
+
+class Footmen :public Unit
+{
+private:
+    /* data */
+public:
+    Footmen(/* args */);
+    ~Footmen();
+};
+
+Footmen::Footmen(/* args */)
+{
+}
+
+Footmen::~Footmen()
+{
+}
+
+
+#endif
---
+++ Units/Slinger.h
@@ -0,0 +1,24 @@
+#ifndef SLINGER_H
+#define SLINGER_H
+
+#include "Unit.h"
+
+class Slinger: public Unit
+{
+private:
+    /* data */
+public:
+    Slinger(/* args */);
+    ~Slinger();
+};
+
+Slinger::Slinger(/* args */)
+{
+}
+
+Slinger::~Slinger()
+{
+}
+
+
+#endif
