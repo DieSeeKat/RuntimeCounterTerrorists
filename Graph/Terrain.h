fd5e39325193ac9d8e4c97dad1f613ede0656a25
FuryOfD <dharshanng@gmail.com>
2022-10-24 11:06:44 +0200

    Finish Doxygen comments

--- Graph/Terrain.h
+++ Graph/Terrain.h
@@ -9,17 +9,32 @@ class Path;
 class Terrain
 {
   public:
+  /// Constructor for Terrain
 
   Terrain();
+  /// Destructor for Terrain
+
   ~Terrain();
   /**
    * @brief Calculate the number of deaths if a Army travels through the Terrain and removes those Units from the Army
    * @param army A pointer to the Army travelling through the Terrain
    */
   void calculate_losses(Army *army);
-  virtual int determineNeededResources(int army_size)=0;
-  virtual int determineNumDeaths(int army_size)=0;
-  virtual Terrain* clone(std::map<void*,void*> &objmap)=0;
+  /**
+   * @brief determines the number of resources needed with respect with the army size
+   * @param army_size
+   * @return the number of resources needed
+   */
+
+  virtual int determineNeededResources(int army_size) = 0;
+  /**
+   * @brief determines the number of deaths once the resources has run out
+   * @param army_size
+   * @return the number of deaths
+   */
+
+  virtual int determineNumDeaths(int army_size)            = 0;
+  virtual Terrain *clone(std::map<void *, void *> &objmap) = 0;
 };
 
 #endif
--- Graph/Terrains/Mountains.h
+++ Graph/Terrains/Mountains.h
@@ -8,17 +8,31 @@
 class Mountains : public Terrain
 {
   public:
-  //------------------------------------------------------ADDED BY DHARSHAN GOPAUL
+  /// Constructor for Mountain
   Mountains();
+  /// Destructor for Mountain
+
   ~Mountains();
+  /**
+   * @brief determines the number of resources needed with respect with the army size
+   * @param army_size
+   * @return the number of resources needed
+   */
+
   int determineNeededResources(int army_size) override;
+  /**
+   * @brief determines the number of deaths once the resources has run out
+   * @param army_size
+   * @return the number of deaths
+   */
+
   int determineNumDeaths(int army_size) override;
   /**
    * @brief Clone method for the Mountains Terrain
    * @param objmap A void to void pointer map
    * @return A pointer to a new cloned Mountains Terrain
    */
-  Terrain* clone(std::map<void*,void*> &objmap);
+  Terrain *clone(std::map<void *, void *> &objmap);
 };
 
 #endif
